7557f97fff01af2f5e12c31df74968e6df7bad70
FacundoCarollo23 <facuscarollo@gmail.com>
2026-01-08 15:55:27 -0300

    FC - Modificaciones angular json y ruteo

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'nuevo-ticket',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'ticket/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
